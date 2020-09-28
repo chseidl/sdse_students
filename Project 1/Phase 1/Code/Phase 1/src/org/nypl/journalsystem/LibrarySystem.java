1c3591575ca77bb5655cbf1ea12e056a68216a9c
Christoph Seidl <chse@itu.dk>
2020-09-28 18:56:29 +0200

    Added material for project 1 phase 1.

---
+++ Project 1/Phase 1/Code/Phase 1/src/org/nypl/journalsystem/LibrarySystem.java
@@ -0,0 +1,41 @@
+package org.nypl.journalsystem;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class LibrarySystem {
+	
+	public LibrarySystem() {
+		//TODO: Initialize system with default journals.
+	}
+	
+	public void load() throws FileNotFoundException, IOException {
+		loadAuthors();
+		loadArticles();
+	}
+	
+	protected void loadAuthors() throws FileNotFoundException, IOException {
+		File file = new File("data/Authors.csv");
+
+		//TODO: Load authors from file 
+	}
+	
+	protected void loadArticles() throws FileNotFoundException, IOException {
+		File file = new File("data/Articles.csv");
+
+		//TODO: Load articles from file and assign them to appropriate journal
+	}
+	
+	
+	public void listContents() {
+		//TODO: Print all journals with their respective articles and authors to the console.
+	}
+	
+	public static final void main(String[] args) throws Exception {
+		LibrarySystem librarySystem = new LibrarySystem();
+		
+		librarySystem.load();
+		librarySystem.listContents();
+	}
+}
