c5e7db56dd9ebccb1739ab04fb1e70a15ad03152
ddeq <ddeq@itu.dk>
2022-10-12 14:25:34 +0200

    Added functionality to record records in a list and collect records in a map for Data Processor

--- Exercises/Java4/CSVProcessor/src/edu/sdse/csvprocessor/CityCSVProcessor.java
+++ Exercises/Java4/CSVProcessor/src/edu/sdse/csvprocessor/CityCSVProcessor.java
@@ -4,18 +4,27 @@ package edu.sdse.csvprocessor;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-
+import java.util.*;
 
 public class CityCSVProcessor {
+	private ArrayList<CityRecord> allRecords;
+	private Map<String, List<CityRecord>> cityRecordsMap;
+	
+	// Constructor
+	public CityCSVProcessor() {
+		allRecords = new ArrayList<>();
+		cityRecordsMap = new HashMap<>();
+	}
 	
 	public void readAndProcess(File file) {
 		//Try with resource statement (as of Java 8)
 		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+
 			//Discard header row
 			br.readLine();
-			
+		
 			String line;
-			
+
 			while ((line = br.readLine()) != null) {
 				// Parse each line
 				String[] rawValues = line.split(",");
@@ -24,12 +33,20 @@ public class CityCSVProcessor {
 				int year = convertToInt(rawValues[1]);
 				String city = convertToString(rawValues[2]);
 				int population = convertToInt(rawValues[3]);
-				
-				//System.out.println("id: " + id + ", year: " + year + ", city: " + city + ", population: " + population);
-				
+								
 				CityRecord cityRecord = new CityRecord(id, year, city, population);
-				System.out.println(cityRecord);
+				allRecords.add(cityRecord); // add to list of all records
+				//System.out.println(cityRecord); // print city record
+				
+				// check if city key in map
+				if (cityRecordsMap.get(city) == null) {
+					// if empty, add data to map
+					cityRecordsMap.put(city, new ArrayList<CityRecord>()); // put key and empty list
+				}
+				cityRecordsMap.get(city).add(cityRecord);
+				System.out.println(cityRecordsMap.get(city));
 			}
+			
 		} catch (Exception e) {
 			System.err.println("An error occurred:");
 			e.printStackTrace();
@@ -63,6 +80,14 @@ public class CityCSVProcessor {
 		
 		reader.readAndProcess(csvFile);
 	}
+	
+	
+	
+	/*private void processRecords {
+		// this will be the processor.... blah blah...
+		//
+		
+	}*/
 }
 
 
--- Exercises/Java4/CSVProcessor/src/edu/sdse/csvprocessor/CityRecord.java
+++ Exercises/Java4/CSVProcessor/src/edu/sdse/csvprocessor/CityRecord.java
@@ -1,26 +1,41 @@
 package edu.sdse.csvprocessor;
 
-public class CityRecord {
-	public int id;
-	public int year;
-	public String city;
-	public int population;
+public class CityRecord { //extends Object is implied
+	private int id; // why set this private, then make a get method?
+	private int year;
+	private String city;
+	private int population;
 	
 	public CityRecord(int id, int year, String city, int population) {
-		this.id = id;
+		this.id = id; // id alone refers to the id in the input, which is why we need this.id
 		this.year = year;
 		this.city = city;
 		this.population = population;
 	}
 	
+	// the toString method is inherited when creating an object class, because object class is an abstract class with this method
+	@Override
 	public String toString() {
 		String s;
-		s = "id: "+this.id +", year: "+this.year+", city: "+this.city+", population: "+ this.population;
+		s = "id: "+ id + ", year: " + year + ", city: " + city +", population: " + population;
 		return s;
 	}
-	//public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	//}
 	
+	public int getID() {
+		return id;
+	}
+	
+	public int getYear() {
+		return year;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	
+	public int getPopulation() {
+		return population;
+	}
+
 }
 
