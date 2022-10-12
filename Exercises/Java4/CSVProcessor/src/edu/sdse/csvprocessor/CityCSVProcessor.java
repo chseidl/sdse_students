7e938782bfee787cadfefb7ecdb139ac144ba138
ddeq <ddeq@itu.dk>
2022-10-12 18:53:14 +0200

    Implemented method to make calculations on city records and print results per city in a formatted string

--- Exercises/Java4/CSVProcessor/src/edu/sdse/csvprocessor/CityCSVProcessor.java
+++ Exercises/Java4/CSVProcessor/src/edu/sdse/csvprocessor/CityCSVProcessor.java
@@ -26,26 +26,34 @@ public class CityCSVProcessor {
 			String line;
 
 			while ((line = br.readLine()) != null) {
-				// Parse each line
+				//Parse each line
 				String[] rawValues = line.split(",");
 				
+				//Get individual values from each line
+				//Convert into appropriate data type
 				int id = convertToInt(rawValues[0]);
 				int year = convertToInt(rawValues[1]);
 				String city = convertToString(rawValues[2]);
 				int population = convertToInt(rawValues[3]);
 								
+				//initialize new record
 				CityRecord cityRecord = new CityRecord(id, year, city, population);
+				
+				//Put data into a record
 				allRecords.add(cityRecord); // add to list of all records
 				//System.out.println(cityRecord); // print city record
 				
 				// check if city key in map
 				if (cityRecordsMap.get(city) == null) {
-					// if empty, add data to map
+					// if empty, add an empty list to the map map
 					cityRecordsMap.put(city, new ArrayList<CityRecord>()); // put key and empty list
 				}
+				
+				// add the record to the map
 				cityRecordsMap.get(city).add(cityRecord);
-				System.out.println(cityRecordsMap.get(city));
+				//System.out.println(cityRecordsMap.get(city));
 			}
+			computeStats(cityRecordsMap);
 			
 		} catch (Exception e) {
 			System.err.println("An error occurred:");
@@ -81,13 +89,37 @@ public class CityCSVProcessor {
 		reader.readAndProcess(csvFile);
 	}
 	
-	
-	
-	/*private void processRecords {
-		// this will be the processor.... blah blah...
-		//
+	public void computeStats(Map<String, List<CityRecord>> cityMap) {
 		
-	}*/
+		for (Map.Entry<String, List<CityRecord>> entry : cityRecordsMap.entrySet()) {
+			//System.out.println(entry.getKey()+entry.getValue());
+			List<CityRecord> recordList = entry.getValue();
+			String city = entry.getKey();
+			int entries = recordList.size();
+			
+			int min = 2050;
+			int max = 0;
+			int popSum = 0;
+			
+			for (CityRecord record : recordList) {
+				System.out.println(record);
+				
+				int year = record.getYear();
+				int population = record.getPopulation();
+				
+				if (year > max) {
+					max = year;
+				}
+				if (year < min) {
+					min = year;
+				}
+				popSum += population;
+			}
+			int avePop = popSum/entries;
+			System.out.println(String.format("Average population of %s (%d-%d; %d Entries): %d ",city,min,max,entries,avePop));
+		}
+	}
+	
 }
 
 
